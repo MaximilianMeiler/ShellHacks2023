526e632003c8da416475ffd850745b832abd196c
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-09-16 01:03:28 -0400

    Inserted canvas display code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 
@@ -12,6 +13,21 @@ function App() {
     localStorage.setItem("key", document.getElementById("keyField").value)
   }
 
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCourses = async () => {
+    setLoading(true);
+    try {
+      // Replace with your Node.js API URL and student token
+      const response = await axios.get('http://localhost:3000/courses');
+      console.log(response.data); 
+      setCourses(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+    setLoading(false);
+  };
 
 
   return (
@@ -24,6 +40,25 @@ function App() {
         <button className="submitKeyButton" onClick={() => submitKey()}>Submit</button>
 
       </header>
+
+
+      <div>
+        <h1>Canvas Courses</h1>
+        <button onClick={fetchCourses}>
+          Fetch Courses
+        </button>
+
+        {loading ? <p>Loading...</p> : <></>}
+
+        <ul>
+          {courses.map((course, index) => (
+            <li key={index}>
+            {course.name}
+            <div dangerouslySetInnerHTML={{ __html: course.syllabus_body || 'No syllabus available' }} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
