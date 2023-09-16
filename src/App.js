77370ca37159018b084b675212f6d336a2f70230
Saurabh <asaurabh2003@gmail.com>
2023-09-16 12:59:40 -0400

    Add Chatbot functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import './App.css';
-import {useEffect, useState, Link} from "react";
+import { useEffect, useState, Link } from "react";
 import axios from "axios";
-
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 function App() {
-  const [key, setKey] = useState(localStorage.getItem("key") === null ? "" : localStorage.getItem("key"));
   document.title = "NaviGator";
+  const [key, setKey] = useState(localStorage.getItem("key") === null ? "" : localStorage.getItem("key"));
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [courseId, setCourseId] = useState();
+  const [messages, setMessages] = useState([
+    {
+      message: 'Hello, how can I help you?',
+      sender: 'Navigator'
+    }
+  ]);
+  const [typing, setTyping] = useState(false);
 
   const submitKey = () => {
     setKey(document.getElementById("keyField").value);
     localStorage.setItem("key", document.getElementById("keyField").value)
     document.getElementById("keyField").value = "";
-    window.scrollTo(0,window.innerHeight * 1/100)   // this is an offense against god
+    window.scrollTo(0, window.innerHeight * 1 / 100)   // this is an offense against god
     setTimeout(() => {
-      window.scrollTo(0,window.innerHeight * 2/100)
+      window.scrollTo(0, window.innerHeight * 2 / 100)
       setTimeout(() => {
-        window.scrollTo(0,window.innerHeight * 3/100)
+        window.scrollTo(0, window.innerHeight * 3 / 100)
         setTimeout(() => {
-          window.scrollTo(0,window.innerHeight * 4/100)
+          window.scrollTo(0, window.innerHeight * 4 / 100)
           setTimeout(() => {
-            window.scrollTo(0,window.innerHeight * 5/100)
+            window.scrollTo(0, window.innerHeight * 5 / 100)
             setTimeout(() => {
-              window.scrollTo(0,window.innerHeight * 6/100)
+              window.scrollTo(0, window.innerHeight * 6 / 100)
               setTimeout(() => {
-                window.scrollTo(0,window.innerHeight * 7/100)
+                window.scrollTo(0, window.innerHeight * 7 / 100)
                 setTimeout(() => {
-                  window.scrollTo(0,window.innerHeight * 8/100)
+                  window.scrollTo(0, window.innerHeight * 8 / 100)
                 }, 5)
               }, 4)
             }, 2)
@@ -37,16 +48,12 @@ function App() {
     fetchCourses();
   }
 
-  const [courses, setCourses] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [courseId, setCourseId] = useState();
-
   const fetchCourses = async () => {
     setLoading(true);
     try {
       // Replace with your Node.js API URL and student token
-      const response = await axios.get(`http://localhost:3500/courses/`, {params: {"key" : key}});
-      console.log(response.data); 
+      const response = await axios.get(`http://localhost:3500/courses/`, { params: { "key": key } });
+      console.log(response.data);
       setCourses(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -55,12 +62,78 @@ function App() {
     console.log(courses);
   };
 
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: 'user',
+      direction: 'outgoing'
+    };
+
+    const newMessages = [...messages, newMessage]; // Add new message to messages array
+
+    // Update messages state
+    setMessages(newMessages);
+
+    // Set typing indicator
+    setTyping(true);
+
+    // Send message to backend
+    await processMessage(newMessages);
+  };
+
+  // !!! This needs to be fixed !!!
+  async function processMessage(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      // let role = "";
+      // if(messageObject.sender === "user") {
+      //   role = "user";
+      // } else {
+      //   role = "assistant";
+      // }
+      let role = messageObject.sender === 'user' ? 'user' : 'assistant';
+      
+      return {
+        role: role,
+        content: messageObject.message
+      }
+    });
+
+    // const systemMessage = {
+    //   role: 'system',
+    //   content: 'Your name is Navigator. Explain all concepts like I am a student in this class.'
+    // }
+
+    // const apiRequestBody = {
+    //   'model': 'gpt-3.5-turbo',
+    //   'messages': [systemMessage, ...apiMessages]
+    // }
+
+    // Fix this with a real api call
+    await fetch('http://localhost:3500/courses', {
+      // method: 'POST',
+      // headers: {
+      //   'Authorizaiton': 'Bearer ' + key,
+      //   'Content-Type': 'application/json'
+      // },
+      // body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json()
+    }).then((data) => {
+      console.log(data);
+      setMessages([...chatMessages, {
+        message: 'navigator message',
+        sender: 'NaviGator'
+      }]);
+      setTyping(false);
+    });
+  }
+
   return (
     <div className="App" id="App" >
       <div className="bg"></div>
       <header className="AppHeader">
         <p className="keyText">
-          Enter 
+          Enter
           <a className="keyTextLink" href="https://community.canvaslms.com/t5/Student-Guide/How-do-I-manage-API-access-tokens-as-a-student/ta-p/273" target="_blank" rel="noreferrer">Canvas Api Key:</a>
         </p>
         <input type="text" id="keyField" className="keyField" placeholder={key}></input>
@@ -72,25 +145,38 @@ function App() {
 
       <div className="canvasDisplay">
         {loading ? <p>Loading...</p> : <></>}
-        {courses.length > 0 ? 
-        <div className='canvasDisplay'>
-          <h1>Canvas Courses</h1>
-          <select className="classDropdown" onChange={(e) => setCourseId(e.target.selectedIndex)}>
-            {courses.map((course) => (
-              <option>{course.name}</option>
-            ))}
-          </select>
+        {courses.length > 0 ?
+          <div>
+            <h1>Canvas Courses</h1>
+            <select className="classDropdown" onChange={(e) => setCourseId(e.target.selectedIndex)}>
+              {courses.map((course) => (
+                <option>{course.name}</option>
+              ))}
+            </select>
 
 
-          {courseId >= 0 ? 
-          <div className='chatBox'>
-            {courses[courseId].name}
-            <div dangerouslySetInnerHTML={{ __html: courses[courseId].syllabus_body || 'No syllabus available' }} />
+            {courseId >= 0 ?
+              <div className='chatBox'>
+                {/* {courses[courseId].name} */}
+                {/* <div dangerouslySetInnerHTML={{ __html: courses[courseId].syllabus_body || 'No syllabus available' }} /> */}
+                <MainContainer>
+                  <ChatContainer>
+                    <MessageList
+                      scrollBehavor='smooth'
+                      typingIndicator={typing ? <TypingIndicator content="NaviGator is typing" /> : null}
+                    >
+                      {messages.map((message, i) => {
+                        return <Message key={i} model={message} />
+                      })}
+                    </MessageList>
+                    <MessageInput placeholder="Type message here" onSend={handleSend} />
+                  </ChatContainer>
+                </MainContainer>
+              </div>
+
+              : <></>}
           </div>
-
-          : <></>}
-        </div>
-        : <h1>No courses found!</h1>
+          : <h1>No courses found!</h1>
         }
 
       </div>
