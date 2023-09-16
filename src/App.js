1680bb1e52e8d65ad97dd35014d4d95f7fcfc9b5
Saurabh <asaurabh2003@gmail.com>
2023-09-16 15:00:13 -0400

    Renamed key to canvasKey variable to avoid confusion, fixed page height

--- src/App.js
+++ src/App.js
@@ -6,34 +6,24 @@ import { MainContainer, ChatContainer, MessageList, Message, MessageInput, Typin
 
 function App() {
   document.title = "NaviGator";
-  const [key, setKey] = useState(localStorage.getItem("key") === null ? "" : localStorage.getItem("key"));
+  const [canvasKey, setCanvasKey] = useState(localStorage.getItem("canvasKey") === null ? "" : localStorage.getItem("canvasKey"));
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [courseId, setCourseId] = useState();
-  const [messages, setMessages] = useState([
-    {
-      message: 'Hello, how can I help you?',
-      sender: 'Navigator'
-    }
-  ]);
+  const [currentCourseIndex, setCurrentCourseIndex] = useState();
+  const [messages, setMessages] = useState([]);
   const [typing, setTyping] = useState(false);
 
   useEffect(() => {
-    if (localStorage.getItem("key").length > 0) {
+    if (localStorage.getItem("canvasKey") && localStorage.getItem("canvasKey").length > 0) {
       fetchCourses();
     }
   }, []);
 
+  const submitCanvasKey = () => {
+    setCanvasKey(document.getElementById("canvasKeyField").value);
+    localStorage.setItem("canvasKey", document.getElementById("canvasKeyField").value)
+    document.getElementById("canvasKeyField").value = "";
 
-  const submitKey = () => {
-    setKey(document.getElementById("keyField").value);
-    localStorage.setItem("key", document.getElementById("keyField").value)
-    document.getElementById("keyField").value = "";
-
-    fetchCourses();
-  }
-
-  const fetchCourses = async () => {
     window.scrollTo(0, window.innerHeight * 1 / 100)   // this is an offense against god
     setTimeout(() => {
       window.scrollTo(0, window.innerHeight * 2 / 100)
@@ -57,17 +47,42 @@ function App() {
       }, 4)
     }, 5)
 
+    fetchCourses();
+  }
+
+  const fetchCourses = async () => {
     setLoading(true);
+
     try {
       // Replace with your Node.js API URL and student token
-      const response = await axios.get(`http://localhost:3500/courses/`, { params: { "key": localStorage.getItem("key") } });
+      const response = await axios.get(`http://localhost:3500/courses/`, {
+        params: {
+          "key": localStorage.getItem("canvasKey")
+        }
+      });
       console.log(response.data);
       setCourses(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
+
     setLoading(false);
     console.log(courses);
+  };
+
+  const handleSelectNewCourse = async (newCurrentCourseIndex) => {
+    console.log(courses)
+    console.log(newCurrentCourseIndex)
+    console.log(courses[newCurrentCourseIndex].name)
+    // Set messages to empty
+    setMessages([
+      {
+        message: 'Hello, how can I help you?',
+        sender: 'Navigator'
+      }
+    ]);
+    // Set course id
+    setCurrentCourseIndex(newCurrentCourseIndex);
   };
 
   const handleSend = async (message) => {
@@ -112,8 +127,8 @@ function App() {
     // !! Fix this with a real api call !!
     await fetch('http://localhost:3500/queryDatabase/', {
       method: "POST",
-      apiKey: localStorage.getItem("key"),
-      courseId: courseId,
+      apicanvasKey: localStorage.getItem("canvasKey"),
+      currentCourseIndex: currentCourseIndex,
       body: JSON.stringify(apiRequestBody)
     }).then((data) => {
       return data.json()
@@ -132,14 +147,14 @@ function App() {
   return (
     <div className="App" id="App" >
       <header className="AppHeader">
-        <p className="keyText">
+        <p className="canvasKeyText">
           Enter
-          <a className="keyTextLink" href="https://community.canvaslms.com/t5/Student-Guide/How-do-I-manage-API-access-tokens-as-a-student/ta-p/273" target="_blank" rel="noreferrer">
+          <a className="canvasKeyTextLink" href="https://community.canvaslms.com/t5/Student-Guide/How-do-I-manage-API-access-tokens-as-a-student/ta-p/273" target="_blank" rel="noreferrer">
             Canvas Api Key:
           </a>
         </p>
-        <input type="text" id="keyField" className="keyField" placeholder={key}></input>
-        <button className="submitKeyButton" onClick={() => submitKey()}>Submit</button>
+        <input type="text" id="canvasKeyField" className="canvasKeyField" placeholder={canvasKey}></input>
+        <button className="submitCanvasKeyButton" onClick={() => submitCanvasKey()}>Submit</button>
       </header>
 
       <div className="canvasDisplay">
@@ -147,17 +162,18 @@ function App() {
         {courses.length > 0 ?
           <div>
             <h1>Canvas Courses</h1>
-            <select className="classDropdown" onChange={(e) => setCourseId(e.target.selectedIndex)}>
+            <select className="classDropdown" onChange={(e) => handleSelectNewCourse(e.target.selectedIndex - 1)}>
+              <option selected disabled hidden>Select a course</option>
               {courses.map((course) => (
                 <option>{course.name}</option>
               ))}
             </select>
 
 
-            {courseId >= 0 ?
+            {currentCourseIndex >= 0 ?
               <div className='chatBox'>
-                {/* {courses[courseId].name} */}
-                {/* <div dangerouslySetInnerHTML={{ __html: courses[courseId].syllabus_body || 'No syllabus available' }} /> */}
+                {/* {courses[currentCourseIndex].name} */}
+                {/* <div dangerouslySetInnerHTML={{ __html: courses[currentCourseIndex].syllabus_body || 'No syllabus available' }} /> */}
                 <MainContainer >
                   <ChatContainer>
                     <MessageList
@@ -165,7 +181,7 @@ function App() {
                       typingIndicator={typing ? <TypingIndicator content="NaviGator is typing" /> : null}
                     >
                       {messages.map((message, i) => {
-                        return <Message key={i} model={message} className="blue" />
+                        return <Message key={i} model={message} />
                       })}
                     </MessageList>
                     <MessageInput placeholder="Ask Navigator..." onSend={handleSend} attachButton={false} />
